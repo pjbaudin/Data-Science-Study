ad5a4a6bedb6f9db3d854340def25c4ced4b30e3
pjbaudin <pierrejean.baudin@hotmail.com>
2017-01-02 12:51:16 +0800

    dataset for week 4

--- Getting_Cleaning_Data/Week 4/Getting_Cleaning_Data_Coursenotes_w4.R
+++ Getting_Cleaning_Data/Week 4/Getting_Cleaning_Data_Coursenotes_w4.R
@@ -69,3 +69,95 @@ paste("Pierre", "Baudin")
 paste0("Pierre", "Baudin")
 
 str_trim("Pierre       ")
+
+
+# Regular expressions
+# a combination of literals and metacharacters
+# text = words
+# metacharacters = grammar
+# 
+# Literals
+# Regular expressions
+# use metacharacters
+# ^text will match the line from the start
+# text$ will match the line from the end
+# Character classes with []
+# combine ^[Ii] am
+# 
+# Specify range of letters
+# [a-z] or [a-zA-Z]
+# ^[0-9][a-zA-Z]
+# 
+# When used at the beginning of a character class
+# the "?" is also a metacharacter and indicates matching
+# characters NOT in the indicated class
+# [^?.]$
+# 
+# Regular expression 2
+# "." is used to refer to any character
+# 9.11
+# 
+# | translates to "or", we can use it to combine two expressions,
+# the sub expressions, teh subexpressions being called alternatives
+# flood|fire
+# 
+# more combine
+# ^[Gg]ood|[Bb]ad
+# ^([Gg]ood|[Bb]ad)
+# [Gg]eorge( [Ww\.)? [Bb]ush
+# 
+# The + and * signs are metacharacters used to indicate repetition
+# * means any number including none of the item
+# + mean at least one of the item
+# (.*) match whatever is in parenthesis
+# 
+# [0-9]= (*.)[0-9]+
+# 
+# { and } are referred to as interval quantifiers
+# They let us specify the minimum and maximum number
+# of matches of an expression
+# [Bb]ush( +[^ ]+ +){1,5} debate
+# 
+# m,n means at least m but not more than n matches
+# m means exactly m matches
+# m, means at least m matches
+# 
+# number of match numbers \1, \2 ...
+#  +([a-zA-Z]+) +1\ +
+#  
+#  The * is "greedy" so it always matches the longest possible
+#  string that satisfies the regular expression
+#  ^s(.*)s
+#  The greediness of * can be turned off with the ? as in:
+#  ^s(.*?)s
+#  
+#  Used with the functions grep, grepl, sub, gsub
+
+
+## Working with dates
+library(lubridate)
+d1 <- date()
+d1
+
+class(d1)
+
+d2 <- Sys.Date()
+d2
+class(d2)
+
+format(d2, "%a %b %d")
+
+# as.Date(x, "%d%M") ...
+# weekdays
+# months(d2)
+attr("origin")
+
+Sys.timezone()
+
+wday(Sys.Date(), label = TRUE)
+
+# Data resources
+# http://www.data.gouv.fr
+# Gapminder
+# Kaggle
+# 
