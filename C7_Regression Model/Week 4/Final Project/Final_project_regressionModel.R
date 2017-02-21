8070e852f059fa761fb3265e9c5a61f62baadf2c
pjbaudin <pierrejean.baudin@hotmail.com>
2017-02-21 16:53:44 +0800

    complete final project and output pdf

--- C7_Regression Model/Week 4/Final Project/Final_project_regressionModel.R
+++ C7_Regression Model/Week 4/Final Project/Final_project_regressionModel.R
@@ -1,1 +1,25 @@
 # Regression Model - Final Project
+
+par(mfcol = c(1,2))
+plot(resid(mpgam_model_step) ~ predict(mpgam_model_step))
+
+qqnorm(rstudent(mpgam_model_step))
+qqline(rstudent(mpgam_model_step), colour = "blue", lwd = 2)
+
+
+ggplot(mtcars) +
+        stat_qq(aes(sample = mpg, colour = factor(cyl)))
+
+summary(mpgam_model_step)
+
+par(mfrow = c(2, 2))
+plot(mpgam_model_step)
+
+par(mfrow = c(2, 2))
+plot(mpgam_model)
+
+par(mfrow = c(2, 2))
+plot(mpgam_model_final)
+
+par(mfrow = c(2, 2))
+plot(mpgam_model_step)
